b0875ab736688611d808053a2cd5377bacce5875
Wi1git <154275447+Wi1git@users.noreply.github.com>
2024-01-09 19:23:14 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Frank Tracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 

acf7525b389233213e68cf66d57982bc10fc71db
Wi1git <154275447+Wi1git@users.noreply.github.com>
2024-01-09 19:19:52 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/IMG_7074.svg" className={styles.logo} />
       </footer>
     </>
   )
